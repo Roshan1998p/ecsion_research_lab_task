0afae003fb7361d52c3b63131371542aa822f0ef
Roshan Punwatkar <104645242+Roshan1998p@users.noreply.github.com>
2024-06-08 01:35:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
-
+const cors = require("cors");
 const app = express();
 const PORT = 6969;
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
